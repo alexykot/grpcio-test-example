ecfad86947f8060c796de917e1727fb6139ec3b4
Alex Kotenko <alex@esqimo.com>
2019-05-27 08:37:02 +0100

    Add example server test
    - added copy of helloworld proto from grpc/grpc
    - added copy of helloworld python grpc server from grpc/grpc
    - added boilerplate
    - added example unittest for Greeter server

---
+++ README.md
@@ -0,0 +1,11 @@
+# grpc_testing hello world example
+_simplest example implementation of gRPC server unit test using python 
+grpcio_testing library_
+
+To run the test execute:
+
+    pipenv install
+    pipenv run python ./example/test_greeter_server.py
+    
+Based on the `grpc/examples/protos/helloworld.proto` from 
+`https://github.com/grpc/grpc`.
---
+++ example/test_greeter_server.py
@@ -0,0 +1,36 @@
+import grpc
+import grpc_testing
+import unittest
+
+from example import helloworld_pb2
+from example.greeter_server import Greeter
+
+
+class TestGreeter(unittest.TestCase):
+    def setUp(self):
+        servicers = {
+            helloworld_pb2.DESCRIPTOR.services_by_name['Greeter']: Greeter()
+        }
+
+        self.test_server = grpc_testing.server_from_dictionary(
+            servicers, grpc_testing.strict_real_time())
+
+    def test_helloworld(self):
+        """ expect to get Greeter response """
+        name = "John Doe"
+        request = helloworld_pb2.HelloRequest(name=name)
+
+        sayhello_method = self.test_server.invoke_unary_unary(
+            method_descriptor=(helloworld_pb2.DESCRIPTOR
+                .services_by_name['Greeter']
+                .methods_by_name['SayHello']),
+            invocation_metadata={},
+            request=request, timeout=1)
+
+        response, metadata, code, details = sayhello_method.termination()
+        self.assertEqual(response.message, f'Hello, {name}!')
+        self.assertEqual(code, grpc.StatusCode.OK)
+
+
+if __name__ == '__main__':
+    unittest.main()
